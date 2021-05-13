510ff55e51dce38881b8aec2a22d6bbd2e72647b
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-05-13 11:56:43 +0300

    Add pagination
    Use kaminari gem to allow paginating results

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,9 @@
 class ArticlesController < ApplicationController
   def index
     articles = Article.recent
+                      .page(params[:page])
+                      .per(params[:per_page])
+
     render json: articles
   end
 
